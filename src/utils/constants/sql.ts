44188e1a10599840147af4301c237d9e0f563c98
Felkon <12007786+FelkonEx@users.noreply.github.com>
2024-07-17 22:59:42 +1000

    Changed insert video query to update on conflict

--- src/utils/constants/sql.ts
+++ src/utils/constants/sql.ts
@@ -1,5 +1,5 @@
 export const INSERT_VIDEO_QUERY: string =
-    "INSERT INTO {0} (video_id, thumbnail_url, title, published_at) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING";
+    "INSERT INTO {0} (video_id, thumbnail_url, title, published_at) VALUES ($1, $2, $3, $4) ON CONFLICT(video_id) DO UPDATE SET thumbnail_url = EXCLUDED.thumbnail_url, title = EXCLUDED.title, published_at = EXCLUDED.published_at;";
 
 export const SELECT_VIDEOS_QUERY: string =
     "SELECT * FROM {0} ORDER BY published_at DESC";
